6ebeabb96945cd1517c5b34c584e8317219853b7
Abhishek Rai <rai.abhishek90@gmail.com>
2014-02-17 15:03:25 +0530

    search.rb Documented

--- controllers/search.rb
+++ controllers/search.rb
@@ -3,14 +3,22 @@ class Search
   include EM::Deferrable
 
   def search(query,domain,id)
+
+    #SEARCH DOMAIN IDENTIFICATION
     if(domain == "all" || domain == "")
       search_domain = ""
     else
       search_domain = domain.to_s + "/"
     end
+    
+    #SEARCH URL
     server_address = 'http://localhost:9200/'
     search_uri = server_address + id.to_s + '/' + search_domain +'_search'
+    
+    #REST CALL TO ELASTIC SEARCH
     data = EM::HttpRequest.new(search_uri).post(body: query.to_json)
+
+    #CALLBACK ON RECEIVED DATA
     data.callback do
       data_response = JSON.parse(data.response)
       puts data_response
